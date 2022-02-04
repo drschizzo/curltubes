a25a250991ecb6ae6059a62209e7e7db3e74fd38
Henri C <henri.casselle@gmail.com>
2022-02-04 17:46:32 +0100

    multiple  TubeBufferGeometry

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import Stats from 'three/examples/jsm/libs/stats.module.js'
 import * as dat from 'dat.gui'
+import SimplexNoise from 'simplex-noise';
+
 
 
 var fragmentshader = require('./shaders/fragment.glsl')
@@ -9,7 +11,45 @@ var vertexshader=require('./shaders/vertex.glsl')
 
 const scene = new THREE.Scene()
 
-    
+const noise=new SimplexNoise()    
+
+
+function computeCurl(x, y, z){
+    var eps = 0.0001;
+  
+    var curl = new THREE.Vector3();
+  
+    //Find rate of change in YZ plane
+    var n1 = noise.noise3D(x, y + eps, z); 
+    var n2 = noise.noise3D(x, y - eps, z); 
+    //Average to find approximate derivative
+    var a = (n1 - n2)/(2 * eps);
+    var n1 = noise. noise3D(x, y, z + eps); 
+    var n2 = noise.noise3D(x, y, z - eps); 
+    //Average to find approximate derivative
+    var b = (n1 - n2)/(2 * eps);
+    curl.x = a - b;
+  
+    //Find rate of change in XZ plane
+    n1 = noise.noise3D(x, y, z + eps); 
+    n2 = noise.noise3D(x, y, z - eps); 
+    a = (n1 - n2)/(2 * eps);
+    n1 = noise.noise3D(x + eps, y, z); 
+    n2 = noise.noise3D(x - eps, y, z); 
+    b = (n1 - n2)/(2 * eps);
+    curl.y = a - b;
+  
+    //Find rate of change in XY plane
+    n1 = noise.noise3D(x + eps, y, z); 
+    n2 = noise.noise3D(x - eps, y, z); 
+    a = (n1 - n2)/(2 * eps);
+    n1 = noise.noise3D(x, y + eps, z); 
+    n2 = noise.noise3D(x, y - eps, z); 
+    b = (n1 - n2)/(2 * eps);
+    curl.z = a - b;
+  
+    return curl;
+  }
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
     camera.position.z = 2
@@ -27,15 +67,13 @@ const scene = new THREE.Scene()
 
     let orbitControls = new OrbitControls(camera, renderer.domElement);
 
-    const geometry = new THREE.SphereBufferGeometry(1, 320, 320)
+  //  let geometry = new THREE.BufferGeometry()
     const material = new THREE.MeshBasicMaterial({
         color: 0x00ff00,
         wireframe: true,
         
     })
 
-
-    //create a shader material
     const shaderMaterial = new THREE.ShaderMaterial({
         uniforms: {
             resolution: {
@@ -49,11 +87,41 @@ const scene = new THREE.Scene()
         fragmentShader: fragmentshader,
     })
 
+    for(let i=0;i<200;i++){
+        let positions = []
+        let start=new THREE.Vector3(Math.random()-.5,Math.random()-.5,Math.random()-.5)
+        positions.push(start)
+        let currentpos=start.clone()
+        for(let j=0;j<500;j++){
+            let curl=computeCurl(currentpos.x,currentpos.y,currentpos.z)
+            currentpos.x+=curl.x*0.0005//*(i+1)
+            currentpos.y+=curl.y*0.0005//*(i+1)
+            currentpos.z+=curl.z*0.0005//*(i+1)
+            positions.push(currentpos.clone())
 
-    const cube = new THREE.Mesh(geometry, shaderMaterial)
-    const particules=new THREE.Points(geometry,shaderMaterial)
-    scene.add(particules)
+            
+        
 
+        }
+        let curve=new THREE.CatmullRomCurve3(positions)
+        
+        
+        let geometry = new THREE.TubeBufferGeometry(curve, 500, 0.01, 10, false);
+
+
+    // geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3))
+
+
+
+        //create a shader material
+        
+
+
+    //  const cube = new THREE.Mesh(geometry, shaderMaterial)
+        // let mesh=new THREE.Mesh(geometry,new THREE.MeshBasicMaterial())
+        let mesh=new THREE.Mesh(geometry,shaderMaterial)
+        scene.add(mesh)
+    }
 
     // window.addEventListener('click', (e) => {
     //     console.log(cube.geometry.getAttribute('position'))
@@ -79,6 +147,9 @@ const scene = new THREE.Scene()
     const stats = Stats()
     document.body.appendChild(stats.dom)
 
+
+   
+
     // const gui = new dat.GUI()
     // const cubeFolder = gui.addFolder('Cube')
     // cubeFolder.add(cube.scale, 'x', -5, 5)
@@ -88,20 +159,21 @@ const scene = new THREE.Scene()
     // const cameraFolder = gui.addFolder('Camera')
     // cameraFolder.add(camera.position, 'z', 0, 10)
     // cameraFolder.open()
-
+    let itime=0
     function animate() {
         requestAnimationFrame(animate)
-      //  cube.rotation.x += 0.01
+      
+       //  cube.rotation.x += 0.01
       //  cube.rotation.y += 0.01
+        itime+=.01
+        let time=itime
+      
+
         
-        let position = cube.geometry.getAttribute('position').array;
-        for(let i=0;i<position.length;i+=3){
-            position[i]+=(Math.random()-.5)*0.01
-            position[i+1]+=(Math.random()-.5)*0.01
-            position[i+2]+=(Math.random()-.5)*0.01
-        }
-        cube.geometry.setAttribute('position', new THREE.Float32BufferAttribute(position, 3))
-        cube.geometry.attributes.position.needsUpdate = true
+
+        
+      //  geometry.setAttribute('position', new THREE.Float32BufferAttribute(newpositions, 3))
+      //  geometry.attributes.position.needsUpdate = true
       
         render()
         orbitControls.update()
